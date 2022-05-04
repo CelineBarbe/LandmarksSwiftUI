53df3537546c4ab3bd244b1441ea618d24428ed4
Céline Barbe <laloutrecosmique@hotmail.fr>
2022-05-04 15:30:14 +0200

    first commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Céline Barbe on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
